3c28fb0e6ebf2e7ae9387d544205d24275873c45
Monica Tuttle <monicatuttle@Monicas-MacBook-Pro.local>
2023-01-15 23:40:07 -0500

    Modified App.js and App.css

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Head(props) {
+  return (
+    <head className="Head">
+      <title>{props.title}</title>
+    </head>
+  )
+
+}
+function Header(props) {
+  return (
+    <header className="Header">
+      <h1>{props.name}</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <nav className="NavBar">
+      <ul>
+        {props.menu_items.map((item) => (
+          <li key={item.id}>
+            {item.title}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>&copy; {props.year} {props.team}</p>
+    </footer>
+  );
+}
+
+const teamMembers = [
+  "Joshua Anderson, ",
+  "Dennis Bowen, ",
+  "Nicholas Hunter, ",
+  "Monica Tuttle",
+]
+
+const items = [
+  <a href="map.html">Map</a>,
+  <a href="settings.html">Settings</a>,
+  <a href="about.html">How It Works</a>
+];
+
+const menuObjects = items.map(
+  (item, i) => ({
+    id: i,
+    title: item
+  })
+);
+
+function SubmitLocation(props) {
+  return (
+    <button>
+      <p>{props.text}</p>
+    </button>
+  )
+  }
+
+function LocateOnMap(props) {
+  return (
+    <button>
+      <p>{props.text}</p>
+    </button>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Head title="Pothole Patrol"/>
+      <Header name="Pothole Patrol" />
+      <Main
+        menu_items={menuObjects}
+      />
+      <SubmitLocation text="Submit My Location"/>
+      <LocateOnMap text="Locate on Map"/>
+      <Footer
+        team={teamMembers}
+        year={new Date().getFullYear()}
+      />
     </div>
   );
 }
