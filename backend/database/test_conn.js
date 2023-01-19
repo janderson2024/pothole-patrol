1714a29fa72094be25d320b70fc4ac0ba8f7125c
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-01-18 23:26:53 -0500

    Users not user

--- backend/database/test_conn.js
+++ backend/database/test_conn.js
@@ -5,7 +5,7 @@ const db = require("./connection");
 
 //This is a very basic example of how to make a query with the database
 async function exampleRoute(){
-    sql = 'SELECT * FROM `user`';
+    sql = 'SELECT * FROM `Users`';
     const [results] = await db.query(sql);
     console.log(results);
 }
@@ -17,8 +17,8 @@ async function exampleRoute(){
 //so db.query('SELECT ? FROM ? WHERE ? = ?', [a,b,c,d]) becomes 'SELECT `a` FROM `b` WHERE `c` = `d`'
 //This is for security so if you need to append user data: DO THIS
 async function exampleRoutePrepared(){
-    const sql = 'SELECT * FROM `user` WHERE `user_id` = ?';
-    const [results] = await db.query(sql, [1]);
+    const sql = 'SELECT * FROM `Users` WHERE `ID` = ? and `city` = ?';
+    const [results] = await db.query(sql, [1, "Las Vegas"]);
     console.log(results);
 }
 
