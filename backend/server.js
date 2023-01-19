7e2fbc59777ce5884fc233c47d962aa2139956d5
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-01-18 22:53:02 -0500

    changed port over 8106 for the server and changed node-fetch version

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+const port = 8106;
 
 app.use(
     bodyParser.json({extended: false}), 
