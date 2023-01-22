f484c974b95abeb683e48a7dfb2ba3de18e758cf
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-01-21 22:25:02 -0500

    First working version of the /user/register api

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,9 @@
-const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
+
+const db = require("../database/connection");
+const latlonDistance = require("../helpers/latlonDistance");
 
 //temporary testing user_register_test
 const path = require("path");
@@ -10,62 +12,152 @@ router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../test_html/user_register_test.html"));
 });
 
-/* NOTES ON IP GEOLOCATION
-https://dev.maxmind.com/geoip/geolite2-free-geolocation-data?lang=en
-  - free database we can download and do geoip based off. 
-  - seems VERY innacurate unless its 100km+ "accuracy range". even then its 73%
+/*Steps:
+    0: make sure the request contains a lat and lon value
+        -return a 400 with a {"error": "need lat and lon"}
 
-https://db-ip.com/db/download/ip-to-city-lite
-  -free database we can download and do geoip off of. 
-  -77% accurate
+    1: check if user already have a valid cookie
+        -if they do: return it back to them and call it good.
 
-https://ip-api.com/docs/api:json
-  -free api 
-  -rate limit of 75 per minute which shouldnt be an issue.
-  -also might not be under "acceptable commercial ussage" but idk
-  -most accurate that I tried
+    2: if they do not have a valid cookie: pull the ip and user agent from the req
 
-  http://ip-api.com/json/[ip]
+    3: make a db seeing if there is already a user with the same ip
+        - if there is more than one, match it by the user-agent
+        - if there is still more than one, return the first one
 
-https://apidocs.geoapify.com/playground/ip-geolocation/
- - uses the ip-to-city-lite...
- */
+        -sidenote: check the last report time, and if less then 1 minute, return a 401 unauthorized
 
+    3: get the city from the ip-api request
 
+    4: match the ip-api lat lon to see if the distance is less than "some amount"
+        -if this isn't true: return a 401 unauthorized
+
+    5: at this step: make a new user, set user-agent, city, ip to their respective values
+        -set reliability_score to be 1, and set last report to "now()" the sql command
+
+    6: return the generated id as a cookie
+*/
 //maybe also take in LAT/LON as a way to make sure the user has location services on
 //takes in user agent, ip address
 //gets the city from the ip address
 //sets reliability to 1
 //last_report is set to now
 
+const COOKIE_NAME = "uID";
+//milli in sec * sec in min * min in hr * hr in day * day in month
+const ONE_MONTH = 1000 * 60 * 60 * 24 * 30;
+
+function makeCookie(res, uid){
+    console.log("Signed in user: " + uid);
+    return res.cookie(COOKIE_NAME, uid, {signed : true, maxAge: ONE_MONTH}).json({"uID" : uid});
+}
+
+async function checkLastSignin(res, sqlResult){
+    //check if the match is spamming this endpoint
+    const last_signin = new Date(sqlResult.last_signin);
+    const now = new Date();
+    const diffBetweenNowAndReport = now.getTime() - last_signin.getTime();
+
+    //checks if the difference is greater than 1 minute
+    if(diffBetweenNowAndReport >= (1000 * 60 * 1)){
+        const updateSQL = "UPDATE `users` SET `last_signin` = now() WHERE `ID` = ?";
+        await db.query(updateSQL, [result.ID]);
+        return makeCookie(res, result.ID);
+    } else {
+        console.log("caught a spam login attempt!");
+        return res.status(401).json({"error": "slow down"});
+    }
+}
+
+
+//register endpoint to get a userID 
+//requires a latitude and longitude in the body
+//returns a uid
+
 router.post("/register", async (req, res) => {
-    //get user IP
+    if(!(req.body.latitude && req.body.longitude)){
+        //lat and lon aren't being sent by client. Reject and move on.
+        return res.status(401).json({"error": "need latitude and longitude"});
+    }
+
+    const userLat = req.body.latitude;
+    const userLon = req.body.longitude;
+
+    //checks if there is a cookie AND it is signed. 
+    //If the cookie isn't properly signed, we cant trust it
+    if(req.signedCookies[COOKIE_NAME]){
+        uid = req.signedCookies[COOKIE_NAME]
+        //cookie is already set and signed, so it's not tampered
+        console.log("Had prev cookie!");
+        return makeCookie(res, uid);
+    }
+
+    //pull the ip and user-agent
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    ua = req.header("user-agent");
+    const userAgent = ua.substring(0, Math.min(200, ua.length));
 
+    //make a db request to see if there is any current user with that IP
+    const checkForUsedIPSQL = "SELECT * from `Users` WHERE `ip` = ?";
+    const [results] = await db.query(checkForUsedIPSQL, [ip]);
+
+    //check results to check for current user with IP
+    if(results.length >= 1){
+        //there is at least one user with the same ip
+        for(result of results){
+            //returns the first one that matches ip and user agent
+            if(result.user_agent == userAgent){
+                //check if the match is spamming this endpoint
+                console.log("Matched userAgent and Ip!");
+                return await checkLastSignin(res, result);
+            }
+        }
+        //if it makes it here: check the time and return the first one
+        console.log("No userAgents matched, but we returned first Ip!");
+        return await checkLastSignin(res, results[0]);
+    }
+
+    //user ip is not in the system, so now its time to create a new user
+
+    //first we want to get the city from the ip
+    //we try the ip-api first. It's free, so I don't feel bad if the results don't line up
     //fetch the ip geolocation data from the ip-api API
-    const apiUrl = "http://ip-api.com/json/" + ip;
+    const apiUrl = "http://ip-api.com/json/"+ ip + "?fields=126975";
     fetchResp = await fetch(apiUrl);
-    ipGeoLocationData = await fetchResp.json();
-    console.log(ipGeoLocationData);
+    ipapiData = await fetchResp.json();
 
-    //get the user agent
-    const userAgent = req.header("user-agent");
-    const userReliability = 1;
+    var city = "";
+    //get the city from the api returned data
+    if(ipapiData.status == "success"){
+        const distance = latlonDistance(ipapiData.lat, ipapiData.lon, userLat, userLon);
+        
+        //if the distance is less than 15 km, then we use this city as the response
+        if(distance > 15){
+            console.log("We set city to the one from ip-api");
+            city = ipapiData.city;
+        }
+    }
 
+    if(!city){
+        //only run if city hasn't been set yet
+        //now we make the call to the geoapify service
+        const geoUrl = "https://api.geoapify.com/v1/geocode/reverse?lat="+ userLat +"&lon=" + userLon + "&format=json&apiKey=" + process.env.GEOAPIFY_KEY;
+        const fetchResp = await fetch(geoUrl);
+        const geoData = await fetchResp.json();
 
-    //testing console.logs
-    console.log("IP: " + ip);
-    //console.log(ipGeoLocationData);
-    console.log("User Agent: " + userAgent);
+        /*console.log("GEOAPIFY data: ");
+        console.log(geoData);*/
+        console.log("We set the city to the one from geoapify");
+        city = geoData.results[0].city;
+    }
 
-    const cookieName = "uID";
-    const uidValue = "example1234";
-    console.log("UID COOKIE: " + req.cookies[cookieName]);
+    //At this point: it is time to insert the new user
+    //new user with a reliability score of 1, a last report time set to 5 min ago (to help dodge the spam detection), and a last signin time of now
+    const insertSql = 'INSERT INTO `Users` (`user_agent`, `city`, `ip`, `reliability`, `last_report`, `last_signin`) VALUES (?, ?, ?, 1, now()-300, now());'
+    const [insertResult] = await db.query(insertSql, [userAgent, city, ip]);
+    console.log("Created new user: " + insertResult.insertId);
 
-    //milli in sec * sec in min * min in hr * hr in day * day in month
-    const oneMonth = 1000 * 60 * 60 * 24 * 30;
-    res.cookie(cookieName, uidValue, {maxAge: oneMonth}).json({cookieName : uidValue})
-
+    return makeCookie(res, insertResult.insertId);
 });
 
 module.exports = router;
