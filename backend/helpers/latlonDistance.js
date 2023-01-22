9066917532dcabce5e0c6c2554d2057b0e6f9b6c
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-01-21 22:30:36 -0500

    forgot the deg2rad function

--- backend/helpers/latlonDistance.js
+++ backend/helpers/latlonDistance.js
@@ -18,4 +18,8 @@ function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
 	return d;
 }
 
+function deg2rad(deg) {
+	return deg * (Math.PI/180)
+}
+
 module.exports = getDistanceFromLatLonInKm;
