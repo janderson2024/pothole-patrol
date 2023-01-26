651bba2c8f37d16c8ebc9bbf4969cb4388942131
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-01-25 21:18:04 -0500

    added a middleware to check for spam and a test api to test the middleware

--- backend/routes/userMiddleware.js
+++ backend/routes/userMiddleware.js
@@ -3,13 +3,55 @@ const fetch = require("node-fetch");
 const db = require("../database/connection");
 const latlonDistance = require("../helpers/latlonDistance");
 
+const COOKIE_NAME = process.env.COOKIE_NAME;
 
 async function userMiddleware(req, res, next){
+    if(!req.signedCookies[COOKIE_NAME]){
+        return res.status(401).json({"error":"no uid provided"});
+    }
+    const UID = req.signedCookies[COOKIE_NAME];
+
+    const getUserSql = "SELECT * FROM `Users` WHERE `ID` = ?";
+    const [result] = await db.query(getUserSql, [UID]);
+    const user = result[0];
+    if(!user){
+        return res.status(501).json({"error":"uid provided, but not in db"});
+    }
+
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+    if(!latitude || !longitude){
+        return res.status(401).json({"error":"No included lat/lon"});
+    }
+
+    const geoUrl = "https://api.geoapify.com/v1/geocode/reverse?lat="+ latitude +"&lon=" + longitude + "&format=json&apiKey=" + process.env.GEOAPIFY_KEY;
+    const fetchResp = await fetch(geoUrl);
+    const geoData = await fetchResp.json();
+
+    geoCity = geoData.results[0].city;
+
+    /*console.log("-----------");
+    console.log(user);
+    console.log(latitude);
+    console.log(longitude);
+    console.log(geoData);
+    console.log("Geoapify city: " + geoCity);*/
+
+    if(user.city != geoCity){
+        return res.status(401).json({"error":"different city than recorded user city... fail quietly"});
+    }
+
+    req.user = user;
+    req.geoData = geoData;
+    req.latitude = latitude;
+    req.longitude = longitude;
+    next();
     //authenticate user and request
 
     //if all good: pass on to next()
 
     //else return error 401
+    next();
 }
 
 module.exports = userMiddleware;
