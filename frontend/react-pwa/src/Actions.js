17cda87a2530689c05de220ffa07090689cb9ade
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-02-01 22:32:57 -0500

    added the Register API call to the Actions.js

--- frontend/react-pwa/src/Actions.js
+++ frontend/react-pwa/src/Actions.js
@@ -1,7 +1,32 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+function getLocation(success, error) {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+async function callRegisterApi(position) {
+  const data = {
+      "latitude": position.coords.latitude,
+      "longitude": position.coords.longitude
+  };
+
+  const response = await fetch('./user/register', {
+      method: 'POST',
+      headers: {
+          'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data),
+  });
+
+  const text = await response.text();
+  console.log(text);
+}
+
 export default function Actions(props) {
+  getLocation(callRegisterApi, (error)=>{console.log(error)});
   return (
     <div className="actions-container">
       <SubmitLocation />
