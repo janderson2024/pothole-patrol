f9951b9cd52dc9b1ce505fd09b804b6a4c19cbd5
Monica Tuttle <monica.tuttle16@gmail.com>
2023-02-08 22:18:49 -0500

    renamed file from react-pwa to react-app. Minor styling changes.

--- frontend/react-app/src/Navbar.js
+++ frontend/react-app/src/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import MenuPopupState from "./PopupMenu";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="nav">
       <h1 className='site-title'>
-        Pothole Patrol
+      <Link to="/">Pothole Patrol</Link>
       </h1>
       <MenuPopupState />
     </nav>
