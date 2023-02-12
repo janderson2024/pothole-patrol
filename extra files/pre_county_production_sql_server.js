a09ff398fb2e56c9c1a409a34432af1979d990ab
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-02-11 22:01:45 -0500

    backup of current sql data

---
+++ extra files/pre_county_production_sql_server.js
@@ -0,0 +1,542 @@
+[
+  {
+    ID: 1,
+    city: 'Port Saint Lucie',
+    zipcode: '34953',
+    report_count: 7,
+    status: 'Not completed',
+    approx_latitude: '27.2621',
+    approx_longitude: '-80.3835'
+  },
+  {
+    ID: 2,
+    city: 'Pittsburgh',
+    zipcode: '15208',
+    report_count: 9,
+    status: 'Not completed',
+    approx_latitude: '40.4490251',
+    approx_longitude: '-79.8999129'
+  },
+  {
+    ID: 3,
+    city: 'Pittsburgh',
+    zipcode: '15217',
+    report_count: 2,
+    status: 'Not completed',
+    approx_latitude: '40.43502505742672',
+    approx_longitude: '-79.91077502644869'
+  },
+  {
+    ID: 4,
+    city: 'Port Saint Lucie',
+    zipcode: '34953',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '27.263022456632413',
+    approx_longitude: '-80.37262992408996'
+  },
+  {
+    ID: 5,
+    city: 'Port Saint Lucie',
+    zipcode: '34983',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '27.31813519980771',
+    approx_longitude: '-80.35417313527839'
+  },
+  {
+    ID: 6,
+    city: 'Port Saint Lucie',
+    zipcode: '34983',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '27.317560721642515',
+    approx_longitude: '-80.36355231435675'
+  },
+  {
+    ID: 7,
+    city: 'Port Saint Lucie',
+    zipcode: '34953',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '27.25648320907977',
+    approx_longitude: '-80.367359718966'
+  },
+  {
+    ID: 8,
+    city: 'Port Saint Lucie',
+    zipcode: '34984',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '27.274570008971093',
+    approx_longitude: '-80.35573553171675'
+  },
+  {
+    ID: 9,
+    city: 'Port Saint Lucie',
+    zipcode: '34984',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '27.289781238411464',
+    approx_longitude: '-80.32987607230446'
+  },
+  {
+    ID: 10,
+    city: 'Port Saint Lucie',
+    zipcode: '34983',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '27.293132974451616',
+    approx_longitude: '-80.32517703035171'
+  },
+  {
+    ID: 11,
+    city: 'Port Saint Lucie',
+    zipcode: '34983',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '27.293402818327976',
+    approx_longitude: '-80.31923385081085'
+  },
+  {
+    ID: 12,
+    city: 'Pittsburgh',
+    zipcode: '15217',
+    report_count: 3,
+    status: 'Not completed',
+    approx_latitude: '40.444595579311525',
+    approx_longitude: '-79.92505945182467'
+  },
+  {
+    ID: 13,
+    city: 'Port Saint Lucie',
+    zipcode: '34953',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '27.262764361205594',
+    approx_longitude: '-80.38983872381638'
+  },
+  {
+    ID: 14,
+    city: 'Des Moines',
+    zipcode: '50311',
+    report_count: 5,
+    status: 'Not completed',
+    approx_latitude: '41.6050926',
+    approx_longitude: '-93.6585769'
+  },
+  {
+    ID: 15,
+    city: 'Des Moines',
+    zipcode: '50311',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '41.59828996656513',
+    approx_longitude: '-93.65698708852405'
+  },
+  {
+    ID: 16,
+    city: 'Des Moines',
+    zipcode: '50310',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '41.600304263724155',
+    approx_longitude: '-93.64816839464496'
+  },
+  {
+    ID: 17,
+    city: 'Port Saint Lucie',
+    zipcode: '34987',
+    report_count: 1,
+    status: 'Not completed',
+    approx_latitude: '27.268706364759655',
+    approx_longitude: '-80.40181175261868'
+  },
+  {
+    ID: 18,
+    city: 'Port Saint Lucie',
+    zipcode: '34953',
+    report_count: 2,
+    status: 'Not completed',
+    approx_latitude: '27.26695163218655',
+    approx_longitude: '-80.37078402768356'
+  }
+];
+[
+  {
+    ID: 1,
+    user_agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    city: 'Port Saint Lucie',
+    ip: '73.0.46.217',
+    reliability: 1,
+    last_report: 2023-02-09T20:03:24.000Z,
+    last_signin: 2023-02-07T01:43:57.000Z
+  },
+  {
+    ID: 2,
+    user_agent: 'Mozilla/5.0 (Android 10; Mobile; rv:109.0) Gecko/109.0 Firefox/109.0',
+    city: 'Pittsburgh',
+    ip: '172.59.137.37',
+    reliability: 1,
+    last_report: 2023-02-09T01:04:09.000Z,
+    last_signin: 2023-02-05T02:08:57.000Z
+  },
+  {
+    ID: 3,
+    user_agent: 'Mozilla/5.0 (Linux; Android 10; SM-G960U1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36',
+    city: 'Pittsburgh',
+    ip: '172.59.136.49',
+    reliability: 1,
+    last_report: 2023-02-10T02:34:52.000Z,
+    last_signin: 2023-02-05T03:42:59.000Z
+  },
+  {
+    ID: 4,
+    user_agent: 'Mozilla/5.0 (Linux; Android 12; SM-G973U1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36',
+    city: 'Willoughby',
+    ip: '98.27.193.224',
+    reliability: 1,
+    last_report: 2023-02-05T12:54:06.000Z,
+    last_signin: 2023-02-05T13:04:58.000Z
+  },
+  {
+    ID: 5,
+    user_agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    city: 'Pittsburgh',
+    ip: '73.154.135.168',
+    reliability: 1,
+    last_report: 2023-02-05T13:05:58.000Z,
+    last_signin: 2023-02-06T21:14:30.000Z
+  },
+  {
+    ID: 6,
+    user_agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    city: 'Des Moines',
+    ip: '75.162.12.135',
+    reliability: 1,
+    last_report: 2023-02-09T04:09:21.000Z,
+    last_signin: 2023-02-09T04:26:36.000Z
+  }
+];
+[
+  {
+    ID: 1,
+    potholeID: 1,
+    userID: 1,
+    timestamp: 2023-02-05T00:46:13.000Z,
+    latitude: '27.2621',
+    longitude: '-80.3835'
+  },
+  {
+    ID: 2,
+    potholeID: 1,
+    userID: 1,
+    timestamp: 2023-02-05T00:50:18.000Z,
+    latitude: '27.2621',
+    longitude: '-80.3835'
+  },
+  {
+    ID: 3,
+    potholeID: 2,
+    userID: 2,
+    timestamp: 2023-02-05T02:08:39.000Z,
+    latitude: '40.4490251',
+    longitude: '-79.8999129'
+  },
+  {
+    ID: 4,
+    potholeID: 3,
+    userID: 2,
+    timestamp: 2023-02-05T02:11:07.000Z,
+    latitude: '40.43502505742672',
+    longitude: '-79.91077502644869'
+  },
+  {
+    ID: 5,
+    potholeID: 3,
+    userID: 2,
+    timestamp: 2023-02-05T02:12:39.000Z,
+    latitude: '40.43514795600851',
+    longitude: '-79.91073131561281'
+  },
+  {
+    ID: 6,
+    potholeID: 1,
+    userID: 1,
+    timestamp: 2023-02-05T03:29:12.000Z,
+    latitude: '27.2621',
+    longitude: '-80.3835'
+  },
+  {
+    ID: 7,
+    potholeID: 4,
+    userID: 1,
+    timestamp: 2023-02-05T03:31:22.000Z,
+    latitude: '27.263022456632413',
+    longitude: '-80.37262992408996'
+  },
+  {
+    ID: 8,
+    potholeID: 2,
+    userID: 3,
+    timestamp: 2023-02-05T03:44:15.000Z,
+    latitude: '40.4490068',
+    longitude: '-79.8998889'
+  },
+  {
+    ID: 9,
+    potholeID: 2,
+    userID: 3,
+    timestamp: 2023-02-05T03:44:15.000Z,
+    latitude: '40.4490068',
+    longitude: '-79.8998889'
+  },
+  {
+    ID: 10,
+    potholeID: 2,
+    userID: 3,
+    timestamp: 2023-02-05T03:51:21.000Z,
+    latitude: '40.4490068',
+    longitude: '-79.8998889'
+  },
+  {
+    ID: 11,
+    potholeID: 5,
+    userID: 1,
+    timestamp: 2023-02-05T17:40:00.000Z,
+    latitude: '27.31813519980771',
+    longitude: '-80.35417313527839'
+  },
+  {
+    ID: 12,
+    potholeID: 6,
+    userID: 1,
+    timestamp: 2023-02-05T17:41:20.000Z,
+    latitude: '27.317560721642515',
+    longitude: '-80.36355231435675'
+  },
+  {
+    ID: 13,
+    potholeID: 7,
+    userID: 1,
+    timestamp: 2023-02-06T00:06:30.000Z,
+    latitude: '27.25648320907977',
+    longitude: '-80.367359718966'
+  },
+  {
+    ID: 14,
+    potholeID: 8,
+    userID: 1,
+    timestamp: 2023-02-06T00:11:06.000Z,
+    latitude: '27.274570008971093',
+    longitude: '-80.35573553171675'
+  },
+  {
+    ID: 15,
+    potholeID: 9,
+    userID: 1,
+    timestamp: 2023-02-06T00:15:59.000Z,
+    latitude: '27.289781238411464',
+    longitude: '-80.32987607230446'
+  },
+  {
+    ID: 16,
+    potholeID: 10,
+    userID: 1,
+    timestamp: 2023-02-06T00:18:00.000Z,
+    latitude: '27.293132974451616',
+    longitude: '-80.32517703035171'
+  },
+  {
+    ID: 17,
+    potholeID: 11,
+    userID: 1,
+    timestamp: 2023-02-06T01:27:52.000Z,
+    latitude: '27.293402818327976',
+    longitude: '-80.31923385081085'
+  },
+  {
+    ID: 18,
+    potholeID: 2,
+    userID: 2,
+    timestamp: 2023-02-06T20:46:16.000Z,
+    latitude: '40.4490003',
+    longitude: '-79.8998829'
+  },
+  {
+    ID: 19,
+    potholeID: 12,
+    userID: 2,
+    timestamp: 2023-02-06T20:47:48.000Z,
+    latitude: '40.444595579311525',
+    longitude: '-79.92505945182467'
+  },
+  {
+    ID: 20,
+    potholeID: 2,
+    userID: 2,
+    timestamp: 2023-02-06T21:04:35.000Z,
+    latitude: '40.4489982',
+    longitude: '-79.8998692'
+  },
+  {
+    ID: 21,
+    potholeID: 12,
+    userID: 2,
+    timestamp: 2023-02-06T21:06:15.000Z,
+    latitude: '40.44433429864565',
+    longitude: '-79.92519425267766'
+  },
+  {
+    ID: 22,
+    potholeID: 12,
+    userID: 2,
+    timestamp: 2023-02-06T21:11:02.000Z,
+    latitude: '40.44446493910553',
+    longitude: '-79.92508399921981'
+  },
+  {
+    ID: 23,
+    potholeID: 13,
+    userID: 1,
+    timestamp: 2023-02-07T01:47:00.000Z,
+    latitude: '27.262764361205594',
+    longitude: '-80.38983872381638'
+  },
+  {
+    ID: 24,
+    potholeID: 1,
+    userID: 1,
+    timestamp: 2023-02-07T03:25:58.000Z,
+    latitude: '27.2621',
+    longitude: '-80.3835'
+  },
+  {
+    ID: 25,
+    potholeID: 14,
+    userID: 6,
+    timestamp: 2023-02-07T03:26:24.000Z,
+    latitude: '41.6050926',
+    longitude: '-93.6585769'
+  },
+  {
+    ID: 26,
+    potholeID: 15,
+    userID: 6,
+    timestamp: 2023-02-07T03:29:37.000Z,
+    latitude: '41.59828996656513',
+    longitude: '-93.65698708852405'
+  },
+  {
+    ID: 27,
+    potholeID: 1,
+    userID: 1,
+    timestamp: 2023-02-07T03:29:44.000Z,
+    latitude: '27.2621',
+    longitude: '-80.3835'
+  },
+  {
+    ID: 28,
+    potholeID: 16,
+    userID: 6,
+    timestamp: 2023-02-07T03:31:49.000Z,
+    latitude: '41.600304263724155',
+    longitude: '-93.64816839464496'
+  },
+  {
+    ID: 29,
+    potholeID: 2,
+    userID: 2,
+    timestamp: 2023-02-07T05:50:54.000Z,
+    latitude: '40.4490059',
+    longitude: '-79.8998676'
+  },
+  {
+    ID: 30,
+    potholeID: 2,
+    userID: 2,
+    timestamp: 2023-02-09T01:03:49.000Z,
+    latitude: '40.449005',
+    longitude: '-79.8998815'
+  },
+  {
+    ID: 31,
+    potholeID: 14,
+    userID: 6,
+    timestamp: 2023-02-09T02:22:28.000Z,
+    latitude: '41.6050762',
+    longitude: '-93.6585783'
+  },
+  {
+    ID: 32,
+    potholeID: 17,
+    userID: 1,
+    timestamp: 2023-02-09T02:22:52.000Z,
+    latitude: '27.268706364759655',
+    longitude: '-80.40181175261868'
+  },
+  {
+    ID: 33,
+    potholeID: 14,
+    userID: 6,
+    timestamp: 2023-02-09T02:49:55.000Z,
+    latitude: '41.6051088',
+    longitude: '-93.6585597'
+  },
+  {
+    ID: 34,
+    potholeID: 1,
+    userID: 1,
+    timestamp: 2023-02-09T02:50:39.000Z,
+    latitude: '27.2621',
+    longitude: '-80.3835'
+  },
+  {
+    ID: 35,
+    potholeID: 18,
+    userID: 1,
+    timestamp: 2023-02-09T03:18:30.000Z,
+    latitude: '27.26695163218655',
+    longitude: '-80.37078402768356'
+  },
+  {
+    ID: 36,
+    potholeID: 14,
+    userID: 6,
+    timestamp: 2023-02-09T03:38:47.000Z,
+    latitude: '41.6051216',
+    longitude: '-93.6585669'
+  },
+  {
+    ID: 37,
+    potholeID: 18,
+    userID: 1,
+    timestamp: 2023-02-09T03:49:17.000Z,
+    latitude: '27.267236539698498',
+    longitude: '-80.3705292306302'
+  },
+  {
+    ID: 38,
+    potholeID: 14,
+    userID: 6,
+    timestamp: 2023-02-09T04:09:01.000Z,
+    latitude: '41.60529363438636',
+    longitude: '-93.65871330731889'
+  },
+  {
+    ID: 39,
+    potholeID: 1,
+    userID: 1,
+    timestamp: 2023-02-09T20:03:04.000Z,
+    latitude: '27.2621',
+    longitude: '-80.3835'
+  },
+  {
+    ID: 40,
+    potholeID: 2,
+    userID: 3,
+    timestamp: 2023-02-10T02:34:32.000Z,
+    latitude: '40.4490125',
+    longitude: '-79.899869'
+  }
+]
