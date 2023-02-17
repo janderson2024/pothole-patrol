2312ba062225737f8f95529e5bccfe255c83f575
Joshua Anderson <joshuaanderson2024@gmail.com>
2023-02-16 20:27:18 -0500

    Updated the testing api calls, and updated the backend tests with them

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -110,30 +110,8 @@ router.get("/potholes", async (req, res) => {
 
 });
 
-router.post("/mid_test", userMiddleware, (req, res) => {
-    console.log("On the api side...");
-    console.log(req.user);
-    console.log(req.geoData);
-    console.log(req.latitude);
-    console.log(req.longitude);
-    res.json({"nerd": "true"});
-});
-
-router.post("/frontend_test", userMiddleware, (req, res) => {
-    console.log("The frontend made a request to this api!");
-
-    res.json({"success": "Data was recieved and will be saved", "lat" : req.body.latitude, "long" : req.body.longitude});
-});
-
-router.get("/show_console", (req, res) => {
-    if(process.env.MODE != "production"){
-        return res.send("You are not on the server. You dont need to access this!");
-    }
-    if(req.cookies["test_html_key"] != process.env.TEST_HTML_KEY){
-        return res.send("Imagine trying to bypass my checks lmaoooooooo");
-    }
-    res.sendFile(path.join(__dirname, "../output.txt"));
-});
+const testingApi = require("./testing");
+router.use("/testing", testingApi);
 
 const routingApi = require("./routing");
 router.use("/routing", routingApi);
